123f91f0832d8f15df9d30b3809b41d6b1df314e
tohyung85 <tantohyung@gmail.com>
2016-11-07 13:49:51 +0000

    create tests for show and edit profile and templates

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,10 @@
 class ProfilesController < ApplicationController
+  before_action :authenticate_user!, only: [:edit]
   def show
     @profile = user_of_profile.profile
+  end
+
+  def edit
   end
 
   private
--- spec/controllers/profiles_controller_spec.rb
+++ spec/controllers/profiles_controller_spec.rb
@@ -3,12 +3,33 @@ require 'rails_helper'
 RSpec.describe ProfilesController, type: :controller do
   let(:user) { FactoryGirl.create(:user) }
   let(:user1) { FactoryGirl.create(:user) }
+
   describe '#show' do
     context 'user signed in or not' do
       render_views
       it 'should show user profile' do
         get :show, id: user.id
         expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe '#edit' do
+    context 'user signed in' do
+      render_views
+      before do
+        sign_in user
+      end
+      it 'should allow user to edit profile if it is his profile' do
+        get :edit, id: user.id
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'user not signed in' do
+      it 'should redirect user to root path' do
+        get :edit, id: user.id
+        expect(response).to redirect_to root_path
       end
     end
   end
